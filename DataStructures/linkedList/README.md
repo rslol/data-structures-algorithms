5ff4cebb7ddf37322bf9b702b9a35e4e66732e5f
Richard Shelton <richard.shelton567@gmail.com>
2020-10-22 16:51:21 -0400

    Amazon Studying

--- DataStructures/linkedList/README.md
+++ DataStructures/linkedList/README.md
@@ -1,5 +1,3 @@
 -- Linked List -- 
-Linked List is a linear structure where each element is a separate entity. Each element (called a node) is comprised of 2 items, 
-the data and a reference to the next node. If the list is empty the reference to the next node will be null. Lined List are a 
-dynamic data structure. 
+Linked List is a linear structure where each element is a separate entity. Each element (called a node) is comprised of 2 items, the data and a reference to the next node. If the list is empty the reference to the next node will be null. Linked List are a dynamic data structure. 
 
--- DataStructures/linkedList/linkedList.js
+++ DataStructures/linkedList/linkedList.js
@@ -4,6 +4,9 @@
 Implement classes Node and Linked Lists
 */
 
+// Steps for Node Class
+// Use a constructor to accept two params, data and nextNode. 
+// If nextNode is not provided, then its default value is null 
 class Node {
     // Node takes 2 params, the data of the node and the node it is linked to 
     // If the second param is not provided, then the default value is null 
@@ -11,13 +14,45 @@ class Node {
         this.data = data;
         this.nextNode = nextNode; 
     }
-
-    
 }
 
 
-class LinkedList {}
+class LinkedList {
+    // For constructor, initialize this.head as null. This.head references to the 
+    // First node of the linked list. 
+    constructor() {
+        this.head = null;
+    }
 
+    insertFirst(data) {
+        // Assign this.head to the newly created node 
+        this.head = new Node(data, this.head); 
+    }
 
+    size() {
+        let count = 0;
+        let node = this.head;
+
+        while(node) {
+            count++;
+            node = node.nextNode; 
+        }
+
+        return count; 
+    }
+
+    getFirst() {
+        return this.head; 
+    }
+
+    getLast() { 
+        let count = 0;
+        let node = this.head
+
+        while(node) {
+            if ()
+        }
+    }
+}
 
 module.exports = { Node, LinkedList }; 
