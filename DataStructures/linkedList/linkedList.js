c6265f9ee7eb3a1a9cf9e5a0fdb10c5c5b4f6da1
Richard Shelton <richard.shelton567@gmail.com>
2020-10-23 13:16:18 -0400

    More LinkedList Methods

--- DataStructures/linkedList/linkedList.js
+++ DataStructures/linkedList/linkedList.js
@@ -18,22 +18,23 @@ class Node {
 
 
 class LinkedList {
-    // For constructor, initialize this.head as null. This.head references to the 
-    // First node of the linked list. 
+    // For constructor, initialize 'this.head' as null. 'This.head' references to the first node of the linked list. 
     constructor() {
         this.head = null;
     }
 
+    // Insert new node at beginning of linked list 
     insertFirst(data) {
         // Assign this.head to the newly created node 
         this.head = new Node(data, this.head); 
     }
 
+    // Figure out how many nodes are in a linked list 
     size() {
         let count = 0;
         let node = this.head;
 
-        while(node) {
+        while (node) {
             count++;
             node = node.nextNode; 
         }
@@ -41,17 +42,37 @@ class LinkedList {
         return count; 
     }
 
+    // Get the first node 
     getFirst() {
         return this.head; 
     }
 
+    // Get the last node 
     getLast() { 
-        let count = 0;
-        let node = this.head
+        let node = this.head;
 
-        while(node) {
-            if ()
+        // Knockout case
+        if (!node) {
+            return null;
+        } 
+
+        while (node) {
+            // If the next node does not exist, return current node
+            if (!node.nextNode) return node;
+            // else make the next node the current node
+            node = node.nextNode;
         }
+    }
+
+    // Remove all nodes from the LinkedList 
+    clear() {
+        // Setting the newest 'this.head' node to null makes the other nodes in the linkedList non-accessible 
+        this.head = null;
+    }
+
+    // Remove the first node 
+    removeFirst() {
+        this.head = this.head.nextNode;
     }
 }
 
